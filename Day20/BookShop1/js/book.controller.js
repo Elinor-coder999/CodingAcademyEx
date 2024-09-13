fdff1fc156b3c8334b3e5864a50c141d685053db
Elinor <israeli.elinor@gmail.com>
2024-09-13 13:16:13 +0300

    implement updatePrice function

--- Day20/BookShop1/js/book.controller.js
+++ Day20/BookShop1/js/book.controller.js
@@ -15,7 +15,7 @@ function renderBooksTable() {
                 <td>${book.price}</td>
                 <td>
                 <button class="read">Read</button>
-                <button class="update">Update</button>
+                <button class="update" onclick="onUpdateBook('${book.id}')">Update</button>
                 <button class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
                 </td>
             </tr>`)
@@ -27,4 +27,15 @@ function onRemoveBook(bookId) {
    const idx = gBooks.findIndex(book => book.id === bookId)
    gBooks.splice(idx,1)
    renderBooksTable()
+}
+
+function onUpdateBook(bookId){
+    const newPrice = +prompt('Enter the new price')
+    updatePrice(bookId, newPrice) 
+}
+
+function updatePrice(bookId, newPrice) {
+   const book = gBooks.find(book=>book.id === bookId)
+    book.price = newPrice
+    renderBooksTable()
 }
